125e3048dc58d5bec1b825249df55710aa6e7784
fionastasi <111540392+fionastasi@users.noreply.github.com>
2022-11-30 17:31:38 -0600

    Update Juego.h

--- Juego.h
+++ Juego.h
@@ -11,12 +11,15 @@
 class Juego
 {
 private:
-    int apuesta();
+    int apuesta;
 public:
     Juego (int);
     int getApuesta();
     int setApuesta(int,int);
 };
+Juego::Juego(int apostar){
+    apuesta=apostar;
+}
 
 int setApuesta(int FichasIniciales, int apuesta)
 {
