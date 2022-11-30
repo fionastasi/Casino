038ed75322a6bcb88eb15b55cb44564f10b1bc5a
fionastasi <111540392+fionastasi@users.noreply.github.com>
2022-11-30 17:30:34 -0600

    Update Casino.cpp

--- Casino.cpp
+++ Casino.cpp
@@ -12,21 +12,42 @@ using namespace std;
 #include "TragaMonedas.h"
 #include "Ruleta.h"
 
-Jugador jugador;
-Credito credito;
-Juego juego;
-TragaMonedas tragaMonedas;
-Ruleta ruleta;
+Jugador jugador("Fiona",1000);
+Fichas fichas(0,0);
+Juego juego(0);
+TragaMonedas tragaMonedas(0);
+Ruleta ruleta(0);
 
 
 int main(){
-    cout<<"Bienvenido al casino, por favor ingresa tu nombre: "<<Jugador.getNombre()<<" e indica cuanto dinero tienes: "<< Jugador.getDineroInicial()<<endl;
-    cin >> nombre;
-    cin >> dineroInicial;
-    cout<<"Perfecto, cuanto de tu dinero deseas cambiar a fichas:"<<Credito.getFichasIniciales()<<endl;
-    cin >> fichasIniciales;
-    cout>>"Desea jugar a la traga monedas (1) o a la ruleta (2)"<<TragaMonedas.getJugar()<<Ruleta.getJugar()<<endl;
-    cin >> TragaMonedas.getJugar();
-    cin >> Ruleta.getJugar();
+    cout<<"Bienvenido al casino, introduce tu nombre"<<endl;
+    string nombre;
+    cin>>nombre;
+    jugador.setNombre(nombre);
+    cout<<"indica cuanto dinero tienes:"<<endl;
+    int cantidadDinero;
+    cin>>cantidadDinero;
+    jugador.setDineroInicial(cantidadDinero,0);
+    cout<<jugador.getNombre();
+    cout<<jugador.getDineroInicial();
+    cout<<"Cuantas fichas quieres?"<<endl;
+    int fichasDeseadas;
+    cin>>fichasDeseadas;
+    fichas.setFichas(cantidadDinero,fichasDeseadas);
+    int opcionJuego;
+    while(true) {
+        cout<<"Desea jugar a la traga monedas (1) o a la ruleta (2)? Selecciona el (3) para salir"<<endl;
+        cin>>opcionJuego;
+        if (opcionJuego==1){
+            cout<<tragaMonedas.setJugar(fichasDeseadas);
+            //fichas.agregarFichas(tragaMonedas.setJugar(fichasDeseadas));
+        }else if (opcionJuego==2){
+            cout<<ruleta.setJugar(fichasDeseadas);
+        }else {
+            cout<<"Gracias por jugar"<<endl;
+        }
+        if (fichasDeseadas==0 || opcionJuego==3){
+            break;
+        }
+    }
 }
-
